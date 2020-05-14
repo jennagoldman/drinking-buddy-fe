8418b95d7290dcbf9bb139e9c9de164fc9be89e1
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-13 22:26:03 -0700

    passing test for handling setBeers action in reducer

--- src/reducers/beerReducer.js
+++ src/reducers/beerReducer.js
@@ -1,9 +1,11 @@
-import { ADD_BEER } from '../actions/beerActions';
+import { ADD_BEER, SET_BEERS } from '../actions/beerActions';
 
 export default function reducer(state = [], action) {
   switch(action.type) {
     case ADD_BEER:
-      return [...state, action.payload]
+      return [...state, action.payload];
+    case SET_BEERS:
+      return action.payload;
     default:
       return state;
   }
--- src/reducers/beerReducer.test.js
+++ src/reducers/beerReducer.test.js
@@ -1,4 +1,4 @@
-import { addBeer } from '../actions/beerActions';
+import { addBeer, setBeers } from '../actions/beerActions';
 import reducer from './beerReducer';
 
 describe('beer reducer', () => {
@@ -24,4 +24,29 @@ describe('beer reducer', () => {
       }
     ]);
   });
+
+  it('handles the SET_BEERS action', () => {
+    const state = [];
+    const action = setBeers([
+      {
+        _id: '1234567',
+        name: 'Jammy Pants',
+        description: 'An ode to all the parents out there and our love of fruit beers. This kettle sour was fermented with \'pant-fulls\' of marionberry, boysenberry and blueberry puree. Drink up now because it\'s time to get your Jammy Pants on!',
+        imageUrl: 'https://untappd.akamaized.net/site/beer_logos/beer-2630231_7e4fd_sm.jpeg',
+        abv: 7
+      }
+    ]);
+
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual([
+      {
+        _id: '1234567',
+        name: 'Jammy Pants',
+        description: 'An ode to all the parents out there and our love of fruit beers. This kettle sour was fermented with \'pant-fulls\' of marionberry, boysenberry and blueberry puree. Drink up now because it\'s time to get your Jammy Pants on!',
+        imageUrl: 'https://untappd.akamaized.net/site/beer_logos/beer-2630231_7e4fd_sm.jpeg',
+        abv: 7
+      }
+    ])
+  })
 });
