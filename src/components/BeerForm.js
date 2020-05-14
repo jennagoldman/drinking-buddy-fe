e60032f50b63c1b7f57677a4bcd458197aaecf60
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-13 19:28:46 -0700

    setup BeerForm component

---
+++ src/components/BeerForm.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const BeerForm = () => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [abv, setAbv] = useState(0);
+  const [ibu, setIbu] = useState(0);
+
+  return (
+    <form>
+      <input type="text" value={name} onChange={({ target }) => setName(target.value)} placeholder="Beer name" />
+      <textarea value={description} onChange={({ target }) => setDescription(target.value)} placeholder="Beer description" />
+      <input type="text" value={imageUrl} onChange={({ target }) => setImageUrl(target.value)} placeholder="Image URL" />
+      <input type="number" min="1" max="100" value={abv} onChange={({ target }) => setAbv(target.value)} placeholder="ABV" />
+      <input type="number" min="5" max="120" value={ibu} onChange={({ target }) => setIbu(target.value)} placeholder="IBU" />
+    </form>
+  )
+};
+
+export default BeerForm;
