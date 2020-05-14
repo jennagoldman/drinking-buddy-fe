881375828550beba54b8921e68d149fca2e50a56
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-13 22:10:24 -0700

    passing test for setBeers action

--- src/actions/beerActions.js
+++ src/actions/beerActions.js
@@ -4,3 +4,9 @@ export const addBeer = beer => ({
   type: ADD_BEER,
   payload: beer
 });
+
+export const SET_BEERS = 'SET_BEERS';
+export const setBeers = beers => ({
+  type: SET_BEERS,
+  payload: beers
+});
--- src/actions/beerActions.test.js
+++ src/actions/beerActions.test.js
@@ -1,7 +1,7 @@
-import { addBeer, ADD_BEER } from './beerActions';
+import { addBeer, ADD_BEER, setBeers, SET_BEERS } from './beerActions';
 
 describe('beers actions', () => {
-  it('it creates a ADD_BEER action', () => {
+  it('creates a ADD_BEER action', () => {
     const action = addBeer({
       _id: '1234567',
       name: 'Jammy Pants',
@@ -19,6 +19,30 @@ describe('beers actions', () => {
         imageUrl: 'https://untappd.akamaized.net/site/beer_logos/beer-2630231_7e4fd_sm.jpeg',
         abv: 7
       }
+    })
+  })
+  it('creates a SET_BEERS action', () => {
+    const action = setBeers([
+      {
+        _id: '1234567',
+        name: 'Jammy Pants',
+        description: 'An ode to all the parents out there and our love of fruit beers. This kettle sour was fermented with \'pant-fulls\' of marionberry, boysenberry and blueberry puree. Drink up now because it\'s time to get your Jammy Pants on!',
+        imageUrl: 'https://untappd.akamaized.net/site/beer_logos/beer-2630231_7e4fd_sm.jpeg',
+        abv: 7
+      }
+    ]);
+
+    expect(action).toEqual({
+      type: SET_BEERS,
+      payload: [
+        {
+          _id: '1234567',
+          name: 'Jammy Pants',
+          description: 'An ode to all the parents out there and our love of fruit beers. This kettle sour was fermented with \'pant-fulls\' of marionberry, boysenberry and blueberry puree. Drink up now because it\'s time to get your Jammy Pants on!',
+          imageUrl: 'https://untappd.akamaized.net/site/beer_logos/beer-2630231_7e4fd_sm.jpeg',
+          abv: 7
+        }
+      ]
     })
   })
 })
