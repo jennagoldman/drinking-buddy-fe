2fd4b3441733213f50ee4529b8fa376092a0b085
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-13 19:03:14 -0700

    connect app to redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App/App';
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
